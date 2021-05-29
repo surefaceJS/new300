aa44826ce564d800036d3fe6718eef19fed2dab8
surefaceJS <surefaceJS@github.com>
2021-05-29 18:01:25 +0500

    hali chala

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// function openNav(index, item) {
+//     if (index >= item) {
+//         document.getElementById("mySidebar").style.width = "200px";
+//         document.getElementById("main").style.marginLeft = "201px";
+//     } else {
+//         document.getElementById("mySidebar").style.width = "0";
+//         document.getElementById("main").style.marginLeft= "0";
+//     }
+  
+// }
+// function secound() {
+//     
+// }
+
+
+
+// const toggleBtn = document.querySelector('#openNav')
+// const mySidebar = document.querySelector('#mySidebar')
+
+// toggleBtn.addEventListener('click', () => {
+//     if (mySidebar.style.display === 'block') {
+//         mySidebar.style.display = 'none';
+//     } else {
+//         mySidebar.style.display === 'block';
+//     }
+// });
+
+
+
+// **********************************  scroll section here  **************************
+// $(document).ready(function () {
+//     $(".openNav").click(function() {
+//       $(".mySidebar").slideToggle()  ;
+//     });
+// });      
+      
